680c5bfc51e5f3a710550c85d7e59aba1bacdffc
Yoni190 <yfisuhu@gmail.com>
2025-02-25 10:43:43 +0300

    Create module for about tab

---
+++ src/about.js
@@ -0,0 +1,13 @@
+function load_about(){
+    const content = document.querySelector('#content');
+    const about = document.createElement('h2');
+    about.textContent = "About Us";
+
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = "At Restaurant, we bring the heart and soul of Ethiopia to your table. Rooted in tradition and rich flavors, our restaurant is dedicated to sharing the warmth of Ethiopian hospitality through authentic, home-cooked meals. <br>Our menu is a celebration of Ethiopia’s diverse culinary heritage, featuring dishes like injera—our signature sourdough flatbread—served with a variety of flavorful stews, lentils, and aromatic spices. Whether you're savoring the bold richness of doro wat, the comforting taste of shiro, or the fresh and vibrant flavors of a traditional vegan platter, every bite tells a story of culture, community, and craftsmanship. <br>At Restaurant, dining is more than just a meal—it’s an experience. We invite you to eat with your hands, share plates with loved ones, and immerse yourself in the time-honored traditions of Ethiopian cuisine. Whether you're a longtime fan of Ethiopian food or trying it for the first time, we welcome you with open arms and a warm, inviting atmosphere.<br>Come join us for a taste of Ethiopia—where every dish is made with love, and every guest is family. "
+
+    content.appendChild(about);
+    content.appendChild(paragraph);
+}
+
+export {load_about}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import {initial_load} from "./content.js";
 import {load_meals} from "./meals.js";
+import {load_about} from "./about.js";
 
 initial_load();
 
-const home = document.querySelector("#home");
+const home = document.querySelector('#home');
 home.addEventListener('click', ()=>{
-    document.querySelector("#content").innerHTML = "";
+    document.querySelector('#content').innerHTML = "";
     initial_load();
-})
+});
 
-const meals = document.querySelector("#meals");
+const meals = document.querySelector('#meals');
 meals.addEventListener('click', ()=>{
-    document.querySelector("#content").innerHTML = "";
+    document.querySelector('#content').innerHTML = "";
     load_meals();
+});
+
+const about = document.querySelector('#about');
+about.addEventListener('click', ()=>{
+    document.querySelector('#content').innerHTML = "";
+    load_about();
 });
