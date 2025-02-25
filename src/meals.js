e87b5b72ade73bd65231724843d25e83403b849c
Yoni190 <yfisuhu@gmail.com>
2025-02-25 10:19:40 +0300

    Create module to load meals tab

---
+++ src/meals.js
@@ -0,0 +1,42 @@
+import shiroPic from "./assets/shiro.jpg";
+import tibsPic from "./assets/tibs.jpeg";
+import genfoPic from "./assets/genfo.jpeg"
+
+
+function load_meals(){
+    const content = document.querySelector("#content");
+    const shiro = document.createElement('div');
+    const tibs = document.createElement('div');
+    const genfo = document.createElement('div');
+
+    const shiroLabel = document.createElement('p');
+    const tibsLabel = document.createElement('p');
+    const genfoLabel = document.createElement('p');
+
+    shiroLabel.textContent = "Shiro";
+    tibsLabel.textContent = "Tibs";
+    genfoLabel.textContent = "Genfo";
+
+    const shiroImage = document.createElement('img');
+    const tibsImage = document.createElement('img');
+    const genfoImage = document.createElement('img');
+
+    shiroImage.src = shiroPic;
+    tibsImage.src = tibsPic;
+    genfoImage.src = genfoPic;
+
+    shiro.appendChild(shiroLabel);
+    shiro.appendChild(shiroImage);
+
+    tibs.appendChild(tibsLabel);
+    tibs.appendChild(tibsImage);
+
+    genfo.appendChild(genfoLabel);
+    genfo.appendChild(genfoImage);
+
+    content.appendChild(shiro);
+    content.appendChild(tibs);
+    content.appendChild(genfo);
+}
+
+export {load_meals};
